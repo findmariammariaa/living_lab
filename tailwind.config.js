564a054df27ac4726b2b9d948d4d0d45c2263f21
Mariam Maria <mariammaria38355@gmail.com>
2025-02-01 03:09:55 +0600

    finished setting up the editor for using tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config.js file.
+
+tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
